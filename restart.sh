5bb4e44e976e53d9209e04b99dba3c0a64142d5d
marchandpatrick <patrick.marchand@cetim.fr>
2020-07-23 14:14:19 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,8 +2,10 @@
 #
 docker network create proxy
 cd ~/host/acme
+# List of site to be updated
 docker-compose up -d; docker-compose logs
-docker exec -it acme --issue --dns dns_azure --server https://acme-staging-v02.api.letsencrypt.org/directory -d static.grinnell.edu -d rootstalk.grinnell.edu -d vaf.grinnell.edu -d vaf-kiosk.grinnell.edu --domain-alias _acme-challenge.leverify.info --key-file /certs/static.grinnell.edu.key --cert-file /certs/static.grinnell.edu.cert --standalone --force
+#docker exec -it acme --issue --dns dns_azure --server https://acme-staging-v02.api.letsencrypt.org/directory -d static.grinnell.edu -d rootstalk.grinnell.edu -d vaf.grinnell.edu -d vaf-kiosk.grinnell.edu --domain-alias _acme-challenge.leverify.info --key-file /certs/static.grinnell.edu.key --cert-file /certs/static.grinnell.edu.cert --standalone --force
+docker exec -it acme --issue --dns dns_cf --server https://acme-staging-v02.api.letsencrypt.org/directory -d mysite1.tk -d *.mysite1.tk -d mysite2.tk -d  --domain-alias _acme-challenge.leverify.info  --standalone --force
 #
 cd ../traefik
 docker-compose up -d; docker-compose logs
